3f21ffa3dfe2a56375fc6129db894fdb650bac7a
jose-lopez <josesmooth@gmail.com>
2025-09-09 15:19:18 -0400

    fixing config issue in the kb generation pipepline's stage

--- scripts/pathwaysJPL-m.pl
+++ scripts/pathwaysJPL-m.pl
@@ -1,30 +1,28 @@
 :-style_check(-discontiguous).
 
-inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind]),(ligand(A);receptor(A)),receptor(B),not(A = B).
+inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind,interact,interaction]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
-final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]).
 
-intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association]).
+intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association,interact,interaction]).
 
-eventoEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+eventoEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]).
 
-finalEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+finalEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]).
 
-%[bind,activate,inactivate,repress,inhibit,regulate,stimulate]
+%[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]
 
 %Patrones con restricciones de objetos
 
-inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind]),(ligand(A);receptor(A)),receptor(B),not(A = B).
+inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind,interact,interaction]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
-final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]).
 
-intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association]).
+intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association,interact,interaction]).
 
-eventoEspecial_rest(A,E,B,L):-transcription_factor(A),buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+eventoEspecial_rest(A,E,B,L):-transcription_factor(A),buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,cause,stimulate,stimulation,prevent,prevention]).
 
 buscar_en_lista(L,[L|_]).
 buscar_en_lista(L,[_|Ys]):-buscar_en_lista(L,Ys).
 
 objeto(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A).
-
-%[trimerize,require,heterodimerize,interact,association,phosphorylate,recruit,dimerize,recognize,participate,activate,inhibit]
--- scripts/pathwaysJPL-r.pl
+++ scripts/pathwaysJPL-r.pl
@@ -1,29 +1,29 @@
 :-style_check(-discontiguous).
 %envento inicio del patron ligando -> receptor o receptor -> receptor
-inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind]),(ligand(A);receptor(A)),receptor(B),not(A = B).
+inicio(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind,interact,interaction]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
 %evento final patron tipo 1, tipo 2  transcription_factor -> objeto_de_cierre
-final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+final(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]).
 
 %eventos intermedios en patron tipo 1 mas tipo 2 que unen el envento inicial con el evento final:  proteina->proteina
-intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association]).
+intermedios(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association,interact,interaction]).
 
 %evento que crea un patron de 2 enventos .. evento inicio, evento cierre (patron tipo 3, tipo 4)
-eventoEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+eventoEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]).
 
 %evento adicional para crear los patrones tipo 5
-finalEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
-%[bind,activate,inactivate,repress,inhibit,regulate,stimulate]
+finalEspecial(A,E,B):-base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]).
+%[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]
 
 %Patrones con restricciones de objetos
 
-inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind]),(ligand(A);receptor(A)),receptor(B),not(A = B).
+inicio_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[association,bind,interact,interaction]),(ligand(A);receptor(A)),receptor(B),not(A = B).
 
-final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+final_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),transcription_factor(A),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]).
 
-intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association]).
+intermedios_rest(A,E,B,L):-buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),protein(B),buscar_en_lista(E,[association,interact,interaction]).
 
-eventoEspecial_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibition,positive_correlation,bind]).
+eventoEspecial_rest(A,E,B,L):-buscar_en_lista(A,L),buscar_en_lista(B,L),base(C),buscar_en_lista(event(A,E,B),C),buscar_en_lista(E,[negative_correlation,inhibit,inhibition,positive_correlation,bind,cause,stimulate,stimulation,prevent,prevention]).
 
 buscar_en_lista(L,[L|_]).
 buscar_en_lista(L,[_|Ys]):-buscar_en_lista(L,Ys).
