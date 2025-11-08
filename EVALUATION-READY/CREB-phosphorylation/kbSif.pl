05105a91b8cc0fc8350f38c562d21dd94c2019bd
jose-lopez <josesmooth@gmail.com>
2025-11-08 19:59:59 -0400

    CREB-phosphorylation-fixed

--- EVALUATION-READY/CREB-phosphorylation/kbSif.pl
+++ EVALUATION-READY/CREB-phosphorylation/kbSif.pl
@@ -9,6 +9,6 @@ event(RPS6KA3, controls-phosphorylation-of, CREB1),
 event(RPS6KA3, controls-state-change-of, CREB1)
 event(RPS6KA5, controls-phosphorylation-of, ATF1),
 event(RPS6KA5, controls-state-change-of, ATF1)
-event(RPS6KA5, controls-phosphorylation-of, ATF1),
-event(RPS6KA5, controls-state-change-of, ATF1)
+event(RPS6KA5, controls-phosphorylation-of, CREB1),
+event(RPS6KA5, controls-state-change-of, CREB1)
 ]).
