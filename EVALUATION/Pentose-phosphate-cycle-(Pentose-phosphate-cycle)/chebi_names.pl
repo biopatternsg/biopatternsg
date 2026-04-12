2757b982251a3ecd99d4cb59c42716b2a64c2a30
jose-lopez <josesmooth@gmail.com>
2026-04-12 10:08:44 -0400

    updating the evaluation tables

--- EVALUATION/Pentose-phosphate-cycle-(Pentose-phosphate-cycle)/chebi_names.pl
+++ EVALUATION/Pentose-phosphate-cycle-(Pentose-phosphate-cycle)/chebi_names.pl
@@ -1,4 +1,4 @@
-chebi_name('chebi:57737', 'D-xylulose 5-phosphate(2-)').
+chebi_name('chebi:57737', 'D-xylulose 5-phosphate(2-)'). | Xylulose-5-P
 chebi_name('chebi:57634', 'beta-D-fructofuranose 6-phosphate(2-)').
 chebi_name('chebi:57259', '2-deoxy-alpha-D-ribose 1-phosphate(2-)').
 chebi_name('chebi:47013', 'D-ribofuranose').
@@ -11,9 +11,11 @@ chebi_name('chebi:78346', 'D-ribofuranose 5-phosphate(2-)').
 chebi_name('chebi:58759', '6-phosphonatooxy-D-gluconate').
 chebi_name('chebi:57720', 'alpha-D-ribose 1-phosphate(2-)').
 chebi_name('chebi:29033', 'iron(2+)').
-chebi_name('chebi:16938', '6-O-phosphono-D-glucono-1,5-lactone').
+chebi_name('chebi:16938', '6-O-phosphono-D-glucono-1,5-lactone'). 6-Phosphogluconolactone
 chebi_name('chebi:18420', 'magnesium(2+)').
-chebi_name('chebi:59776', 'D-glyceraldehyde 3-phosphate(2-)').
+chebi_name('chebi:59776', 'D-glyceraldehyde 3-phosphate(2-)'). | 3-Phosphoglyceraldehyde | glyceraldehyde 3-phosphate
 chebi_name('chebi:58225', 'alpha-D-glucose 6-phosphate(2-)').
 chebi_name('chebi:57651', '2-deoxy-D-ribose 5-phosphate(2-)').
 chebi_name('chebi:58121', 'D-ribulose 5-phosphate(2-)').
+
+In the context of chemical compounds find synonyms for D-xylulose 5-phosphate(2-)
