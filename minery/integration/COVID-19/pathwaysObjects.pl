22f035eb9386d49a57ab11a81c55b31aeaa4af67
jose-lopez <josesmooth@gmail.com>
2026-08-31 14:12:04 -0400

    activation of the integration of experiments functionality

---
+++ minery/integration/COVID-19/pathwaysObjects.pl
@@ -0,0 +1,53 @@
+% Below the lines for the objects roles from the user and the first step of the pipeline.
+
+ligand('CXCR4').
+protein('CXCR4').
+protein('PIAS1').
+receptor('CCR5').
+protein('CCR5').
+protein('ACE2').
+enzyme('ACE2').
+receptor('CD4').
+protein('CD4').
+protein('IPO9').
+protein('CD8B').
+protein('PPP1R10').
+protein('IPO7').
+protein('MICA').
+protein('ORF6').
+protein('IPO13').
+protein('IPO5').
+protein('IPO4').
+protein('IPO8').
+protein('HLA-C').
+protein('PIAS3').
+protein('PIAS2').
+protein('PIAS4').
+protein('NMI').
+protein('ORF8').
+protein('JAK3').
+enzyme('JAK3').
+protein('HLA-B').
+protein('MICB').
+protein('IPO5P1').
+enzyme('PSMB7').
+protein('IPO11').
+protein('PRRC2B').
+protein('MICD').
+% Below the lines for the objects roles from pubtator.
+% ligands from PubTator
+% genes from PubTator
+% diseases from PubTator
+% variant from PubTator
+% species from PubTator
+% cell lines from PubTator
+% The following lines are to avoid errors in the process: They should not be modified.
+% Las siguientes lineas son para evitar errores en el proceso: No deben ser modificadas.
+enzyme('').
+protein('').
+transcription_factor('').
+receptor('').
+ligand('').
+disease('').
+transcription_factor('SRY').
+protein('SRY').
